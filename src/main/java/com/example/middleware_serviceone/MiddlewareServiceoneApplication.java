6fee040cefad3702f9fff9c9a9921934d82c9d90
Milan Sangani <milan.sangani@jbl.local>
2025-07-26 10:26:24 +0530

    Initial commit: Java middleware service

---
+++ src/main/java/com/example/middleware_serviceone/MiddlewareServiceoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.middleware_serviceone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareServiceoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareServiceoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/middleware_serviceone/MiddlewareServiceoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.middleware_serviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareServiceoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
